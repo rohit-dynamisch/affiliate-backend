c508dd53deb4c9c4c7a6474189a7f75d992c3e12
rohit-dynamisch <rohit.badhai@dynamisch.co>
2025-07-10 12:04:57 +0530

    vercel changes

--- src/app.js
+++ src/app.js
@@ -5,11 +5,7 @@ const { config } = require('./config/config');
 const cors = require('cors'); // 👈 import cors
 
 const app = express();
-app.use(cors({
-  origin: '*', // 👈 React app's URL
-  methods: ['GET', 'POST'],        // Allowed methods
-  credentials: true                // If you're sending cookies or auth headers
-}));
+app.use(cors());
 app.use(express.json());
 
 // Mount routes
